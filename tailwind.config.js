912be668031906eb3262d703ba5e15078079e3ee
dawidseipold <seipold.dawid@gmail.com>
2024-12-30 01:40:55 +0100

    build: init commit

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+import plugin from 'tailwindcss'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+      colors: {
+        custom: {
+          background: {
+            primary: "#0b0b0b",
+            secondary: "#171717",
+            tertiary: "#282828"
+          }
+        }
+      }
+    },
+  },
+  plugins: [
+    plugin(function({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
+}
+
