69ee81dc25e7edadf41cb2a9f6df591f49338b41
dawidseipold <seipold.dawid@gmail.com>
2025-01-06 06:49:57 +0100

    feat: add a form to control bingo grid size

---
+++ src/components/forms/GridSizeForm.tsx
@@ -0,0 +1,81 @@
+import { Select } from "@kobalte/core/select"
+import { createForm } from "@tanstack/solid-form"
+import { FaSolidCheck as Check, FaSolidChevronDown as ChevronDown } from 'solid-icons/fa'
+import { changeSize, Size, sizeSchema } from "../../stores/bingo"
+import { IoClose as X } from 'solid-icons/io'
+import * as v from 'valibot';
+
+export const GridSizeForm = () => {
+  const form = createForm<Size>(() => ({
+    defaultValues: {
+      x: 5,
+      y: 5
+    },
+    onSubmit: async ({ value }) => {
+      changeSize(value)
+    }
+  }))
+
+  return (
+    <form onSubmit={(e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      form.handleSubmit();
+    }}>
+      <div class="flex items-center gap-x-2">
+        <h3 class="font-semibold">Size</h3>
+
+        <form.Field
+          name='x'
+          validators={{
+            onChange: v.pipe(
+              v.number(),
+              v.minValue(1, "Size must be at least 1"),
+              v.maxValue(15, "Size can't be bigger than 15")
+            ),
+          }}
+          children={(field) => (
+            <input
+              type="number"
+              min="1"
+              max="15"
+              name={field().name}
+              value={field().state.value}
+              onBlur={field().handleBlur}
+              onChange={(e) => {
+                const value = Math.min(Math.max(Number(e.currentTarget.value), 1), 15);
+                field().handleChange(value);
+
+                form.handleSubmit();
+              }}
+              class="ml-auto w-16 flex justify-center text-center items-center px-4 h-10 bg-background-300 border-2 border-background-100 text-black rounded-2xl [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+            />
+          )}
+        />
+
+        <span><X /></span>
+
+        <form.Field
+          name='y'
+          children={(field) => (
+            <input
+              type="number"
+              min="1"
+              max="15"
+              name={field().name}
+              value={field().state.value}
+              onBlur={field().handleBlur}
+              onChange={(e) => {
+                const value = Math.min(Math.max(Number(e.currentTarget.value), 1), 15);
+                field().handleChange(value);
+
+                form.handleSubmit();
+              }}
+              class="w-16 flex justify-center text-center items-center px-4 h-10 bg-background-300 border-2 border-background-100 text-black rounded-2xl [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+            />
+          )}
+        />
+      </div>
+    </form>
+  )
+}
