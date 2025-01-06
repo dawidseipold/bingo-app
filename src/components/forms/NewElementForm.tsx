b1e57219a3275c7e0387c0c8411b10730721d19e
dawidseipold <seipold.dawid@gmail.com>
2025-01-06 06:50:19 +0100

    refactor: rewrite new element form

--- src/components/forms/NewElementForm.tsx
+++ src/components/forms/NewElementForm.tsx
@@ -5,12 +5,14 @@ import { FaSolidArrowRightLong as ArrowRight } from 'solid-icons/fa'
 export const NewElementForm = () => {
   const form = createForm<Item>(() => ({
     defaultValues: {
+      id: "",
       value: ""
     },
     onSubmit: async ({ value, formApi }) => {
-      addItem(value)
+      value.id = crypto.randomUUID();
+      addItem(value);
 
-      formApi.reset()
+      formApi.reset();
     }
   }))
 
@@ -23,28 +25,31 @@ export const NewElementForm = () => {
       }}
       class="flex flex-col gap-y-1"
     >
-      <h3 class="font-semibold">Add new element</h3>
+      <form.Field
+        name="value"
+        children={(field) => (
+          <>
+            <label for={field().name} class="font-semibold">Add new element</label>
 
-      <div class="flex items-center gap-x-2 h-10 w-full">
-        <form.Field
-          name="value"
-          children={(field) => (
-            <input
-              name={field().name}
-              id={field().name}
-              value={field().state.value}
-              onBlur={field().handleBlur}
-              onInput={(e) => field().handleChange(e.target.value)}
-              class="flex items-center px-4 h-full w-full bg-background-300 border-2 border-background-100 text-black rounded-2xl"
-            />
-          )}
-        />
+            <div class="flex items-center gap-x-2 h-10 w-full">
+              <input
+                name={field().name}
+                id={field().name}
+                value={field().state.value}
+                onBlur={field().handleBlur}
+                onInput={(e) => field().handleChange(e.target.value)}
+                class="flex items-center px-4 h-full w-full bg-background-300 border-2 border-background-100 text-black rounded-2xl"
+              />
 
-        {/* TODO: Add a custom action button class */}
-        <button type="submit" class="bg-black rounded-xl h-full text-white aspect-square flex items-center justify-center">
-          <ArrowRight />
-        </button>
-      </div>
+              {/* TODO: Add a custom action button class */}
+              <button type="submit" class="bg-black rounded-xl h-full text-white aspect-square flex items-center justify-center">
+                <ArrowRight />
+              </button>
+            </div>
+          </>
+        )}
+      />
+
 
     </form>
   )
