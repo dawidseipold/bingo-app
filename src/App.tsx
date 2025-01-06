d8649a2bc174460972e5a2ea51da28f5134a714a
dawidseipold <seipold.dawid@gmail.com>
2025-01-06 06:51:35 +0100

    chore: remove unused code from the app component and prepare it for the new components

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,10 @@ import { Tile } from './components/Tile';
 import { items as itemsStore } from './stores/items';
 import { shuffle } from './utils/shuffle';
 import { Menu } from './components/Menu';
+import { bingo } from './stores/bingo';
 
 function App() {
   const items = () => itemsStore.items;
-  const [shuffledItems, setShuffledItems] = createSignal(items());
-  const [emptyMiddle, setEmptyMiddle] = createSignal(false);
-
-  createEffect(() => {
-    setShuffledItems(items());
-  });
-
-  const gridSize = () => {
-    const length = items().length + (emptyMiddle() ? 1 : 0);
-    return Math.ceil(Math.sqrt(length));
-  };
-
-
-  const shuffleItems = () => {
-    setShuffledItems(items());
-    let shuffled = shuffle(itemsStore.items);
-
-    const totalElements = shuffled.length + (emptyMiddle() ? 1 : 0);
-    const gridSize = Math.ceil(Math.sqrt(totalElements));
-    const gridLength = gridSize * gridSize;
-
-    if (emptyMiddle() && gridSize % 2 !== 0) {
-      const middleIndex = Math.floor(gridLength / 2);
-      shuffled.splice(middleIndex, 0, { value: "" });
-    }
-
-    while (shuffled.length < gridLength) {
-      shuffled.push({ value: "" });
-    }
-
-    setShuffledItems(shuffled);
-  };
 
   return (
     <main class="flex flex-col gap-y-8 p-8 items-center min-h-dvh bg-background-100">
@@ -46,23 +15,22 @@ function App() {
 
       <div class="flex gap-x-8 w-full">
         <div class="flex w-full section">
-          s
-          <Show when={shuffledItems().length > 0}>
-            <div
-              style={{
-                display: "grid",
-                "grid-template-columns": `repeat(${gridSize()}, 1fr)`,
-                "grid-template-rows": `repeat(${gridSize()}, 1fr)`
-              }}
-              class="w-max h-max gap-4 bg-blue-400/25 rounded-3xl p-4"
-            >
-              <For each={shuffledItems()}>
-                {(item) => (
-                  <Tile text={item.value} />
-                )}
-              </For>
-            </div>
-          </Show>
+          {/* <Show when={shuffledItems().length > 0}> */}
+          <div
+            style={{
+              display: "grid",
+              "grid-template-columns": `repeat(${bingo.size.x}, 1fr)`,
+              "grid-template-rows": `repeat(${bingo.size.y}, 1fr)`
+            }}
+            class="w-max h-max gap-4 bg-blue-400/25 rounded-3xl p-4"
+          >
+            <For each={[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}>
+              {(item) => (
+                <Tile text={"test"} />
+              )}
+            </For>
+          </div>
+          {/* </Show> */}
         </div>
 
         <Menu />
