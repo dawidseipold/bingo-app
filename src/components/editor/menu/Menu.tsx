16cc25aaf5f0d35451ca98b861e87b5cbd240217
dawidseipold <seipold.dawid@gmail.com>
2025-01-14 11:26:11 +0100

    feat: add basic functionalities from the old menu to the new one and add expanding ability

--- src/components/editor/menu/Menu.tsx
+++ src/components/editor/menu/Menu.tsx
@@ -1,24 +1,36 @@
-import { Component, Match, Switch } from "solid-js";
-import { FaSolidAnglesLeft as DoubleChevron } from 'solid-icons/fa'
+import { Component, Match, Setter, Show, Switch } from "solid-js";
+import { FaSolidAnglesLeft as DoubleChevron, FaSolidPlus as Plus } from 'solid-icons/fa'
+import { cn } from "../../../utils/cn";
+import { ToggleButton } from "@kobalte/core/toggle-button";
+import { GridSizeForm } from "../../forms/GridSizeForm";
+import { ElementsList } from "../../ElementsList";
+import { NewElementForm } from "../../forms/NewElementForm";
 
 interface EditorMenuProps {
   expanded: boolean
+  setExpanded: Setter<boolean>
 }
 
 export const EditorMenu: Component<EditorMenuProps> = (props) => {
-  const expanded = () => props.expanded;
 
   return (
-    <div>
-      <Switch>
-        <Match when={props.expanded}>
-          Expanded
-        </Match>
+    <aside class="fixed top-8 right-8 flex flex-col gap-y-2 max-h-[calc(100dvh-4rem)] h-max rounded-2xl p-1 bg-background-100">
+      <ToggleButton
+        pressed={props.expanded}
+        onChange={props.setExpanded}
+        aria-label="menu-state"
+        class={cn("flex items-center justify-center p-3.5 rounded-2xl hover:bg-background-200", { ["w-max"]: props.expanded })}
+      >
+        <DoubleChevron class={cn({ ["rotate-180"]: props.expanded })} />
+      </ToggleButton>
 
-        <Match when={!props.expanded}>
-          Shrunk
-        </Match>
-      </Switch>
-    </div>
+      <Show when={props.expanded}>
+        <div class="flex flex-col gap-y-4 p-2 overflow-scroll">
+          <GridSizeForm />
+          <NewElementForm />
+          <ElementsList />
+        </div>
+      </Show>
+    </aside>
   )
 }
