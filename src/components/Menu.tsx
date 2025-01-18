9f4747e8a19c794e8e03122391793a9a8108ef52
dawidseipold <seipold.dawid@gmail.com>
2025-01-18 23:09:07 +0100

    chore: remove unused tooltip code

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -51,20 +51,6 @@ export const Menu = () => {
           2
         </Tabs.Content>
       </Tabs>
-
-      {/* <Tooltip openDelay={100} closeDelay={50}> */}
-      {/*   <Tooltip.Trigger class="p-1.5 border-[3px] border-stone-600 hover:border-stone-600/75 rounded-xl group"> */}
-      {/*     <Plus class="w-5 h-5 text-stone-600 group-hover:text-stone-600/75" /> */}
-      {/*   </Tooltip.Trigger> */}
-
-      {/*   <Tooltip.Portal> */}
-      {/*     <Tooltip.Content class="bg-stone-700 text-white tracking-wide font-semibold px-4 py-2 rounded-2xl mt-2"> */}
-      {/*       Add new */}
-      {/*     </Tooltip.Content> */}
-      {/*   </Tooltip.Portal> */}
-      {/* </Tooltip> */}
-
-      {/* <button onClick={shuffleItems} class="flex items-center justify-center border-4 border-blue-500 hover:border-blue-500/75 rounded-3xl p-4 text-blue-500 hover:text-blue-500/75 font-bold tracking-wider">Shuffle</button> */}
     </aside >
   )
 }
